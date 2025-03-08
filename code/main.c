d04a280ca322d162e8801753fc7b211608d91669
holychowders <theholychowders@gmail.com>
2025-03-08 01:30:24 -0500

    Get a single basic case working

--- code/main.c
+++ code/main.c
@@ -8,8 +8,10 @@ typedef int32_t bool;
 
 // NOTE: THE OFFSET IS DIFFERENT FOR DIFFERENT LEVELS
 // Flag: bNoThermalAvailable (available in the first two levels of the game)
-const size_t bNoThermalAvailable_offset = 0x0000182B;
-const int bNoThermalAvailable_size = 19;
+static const int nothermal_flag_size = 19;
+static const size_t nothermal_offset = 0x0000182B;
+static const char nothermal_original_bytes[nothermal_flag_size] = "bNoThermalAvailable";
+static const char nothermal_patchbytes[nothermal_flag_size] = {0x00};
 
 int main(void) {
     puts("Splinter Cell Basic Hacks Trainer");
@@ -18,48 +20,28 @@ int main(void) {
     { // Training level part 1
         FILE* f_training_level_part_1 = {0};
         // FIXME: CLOSE FILE
-        if (0 == fopen_s(&f_training_level_part_1, s_training_level_part_1, "rb")) {
+        if (0 == fopen_s(&f_training_level_part_1, s_training_level_part_1, "r+b")) {
             puts("INFO: Opened training level part 1");
-            if (0 == fseek(f_training_level_part_1, 0, SEEK_END)) {
-                puts("INFO: SEEK_END successful");
-                long size = ftell(f_training_level_part_1);
-                rewind(f_training_level_part_1);
-                if (size > 0) {
-                    printf("INFO: Size of level file: %ld\n", size);
-                    char bytes[size];
-                    size_t objects_read = fread(bytes, size, 1, f_training_level_part_1);
-                    if (objects_read == 1) {
-                        puts("INFO: We have successfully read in part 1 of the training level");
+            if (0 == fseek(f_training_level_part_1, nothermal_offset, SEEK_SET)) {
+                puts("INFO: bNoThermalAvailable flag offset seek");
 
-                        /************************
-                        ** MEMORY PATCHES HERE **
-                        *************************/
-
-
-
-
-#if 0  // Let's not try to do this dynamically. The offset should never ever change. Just hardcode the memory patch.
-                        const char thermal_flag[] = "bNoThermalAvailable";
-                        const int thermal_flag_size = 19;
-                        bool matching_thermal_flag = false;
-                        for (u8 ibyte = 0; ibyte < size; ++ibyte) {
-                            printf("Byte Index: %d out of %d\n", ibyte, size);
-                            if (size - thermal_flag_size > 0) { // If there's still room for the flag exist
-                                for (u8 i_possible_byte = 0; i_possible_byte < thermal_flag_size; ++i_possible_byte) { // Search the next X bytes for the flag
-                                    if (bytes[i_possible_byte] == thermal_flag[i_possible_byte]) {
-                                        matching_thermal_flag = true;
-                                        puts("matched");
-                                    } else {
-                                        puts("failed match");
-                                        matching_thermal_flag = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-#endif
-                    }
+                /** ** MEMORY PATCHES HERE ** **/
+                const bool patch = true;
+                const bool restore = false;
+                size_t objects_written = {0};
+                if (patch) {
+                    objects_written = fwrite(&nothermal_patchbytes, sizeof(nothermal_patchbytes[0]), nothermal_flag_size, f_training_level_part_1);
+                } else if (restore) {
+                    objects_written = fwrite(&nothermal_original_bytes, sizeof(nothermal_original_bytes[0]), nothermal_flag_size, f_training_level_part_1);
                 }
+
+                if (objects_written == nothermal_flag_size) {
+                    puts("INFO: Patched bNoThermalAvailable");
+                } else {
+                    puts("fail");
+                }
+
+                rewind(f_training_level_part_1);
             }
         }
     }
