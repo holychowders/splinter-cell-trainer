f3d4b1057f065ac4b12b23cf8bb55551331c88cf
holychowders <theholychowders@gmail.com>
2025-03-08 23:08:04 -0500

    Patch thermal for all levels

--- code/main.c
+++ code/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 
 /** ** GENERIC TYPES ** **/
 typedef uint8_t u8;
@@ -18,40 +19,39 @@ static const char level_path_training_3[] =
 
 // Tbilisi
 static const char level_path_tbilisi_0[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_1_0_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_1_0Tbilisi.scl";
 static const char level_path_tbilisi_1[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_1_1_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_1_1Tbilisi.scl";
 static const char level_path_tbilisi_2[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_1_2_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_1_2Tbilisi.scl";
 
 // Defense Ministry
 static const char level_path_defense_ministry_1[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_2_1_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_2_1DefenseMinistry.scl";
 static const char level_path_defense_ministry_2[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_2_2_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_2_2DefenseMinistry.scl";
 
 // Caspian Oil Refinery
 static const char level_path_caspian_oil_refinery_2[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_3_2_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_3_2CaspianOilRefinery.scl";
 static const char level_path_caspian_oil_refinery_3[] =
-    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_3_3_Tbilisi.scl";
+    "C:/Program Files (x86)/Steam/steamapps/common/Splinter Cell/maps/1_3_3CaspianOilRefinery.scl";
 
 /** ** NOTHERMAL FLAG OFFSETS ** **/
+#define NOTHERMAL_FLAG_OFFSET_COUNT 9
 #define NOTHERMAL_FLAG_OFFSET_TRAINING_2 0x0000182B
 #define NOTHERMAL_FLAG_OFFSET_TRAINING_3 0x00001B72
-
 #define NOTHERMAL_FLAG_OFFSET_TBILISI_0 0x00002689
 #define NOTHERMAL_FLAG_OFFSET_TBILISI_1 0x000034AD
 #define NOTHERMAL_FLAG_OFFSET_TBILISI_2 0x00002D4F
-
 #define NOTHERMAL_FLAG_OFFSET_DEFENSE_MINISTRY_1 0x000038AE
 #define NOTHERMAL_FLAG_OFFSET_DEFENSE_MINISTRY_2 0x00003624
-
 #define NOTHERMAL_FLAG_OFFSET_CASPIAN_OIL_REFINERY_2 0x00001FFB
 #define NOTHERMAL_FLAG_OFFSET_CASPIAN_OIL_REFINERY_3 0x000037E7
 
 /** ** GAME FLAG STRINGS ** **/
 #define NOTHERMAL_FLAG_SIZE 19
+#define NOTHERMAL_TOTAL_SIZE_TO_WRITE (size_t)(NOTHERMAL_FLAG_SIZE * NOTHERMAL_FLAG_OFFSET_COUNT)
 static const char nothermal_flag_original_bytes[NOTHERMAL_FLAG_SIZE] = "bNoThermalAvailable";
 static const char nothermal_flag_patch_bytes[NOTHERMAL_FLAG_SIZE] = {0};
 
@@ -65,7 +65,7 @@ static bool streq(char* str1, char* str2) {
 }
 
 /** ** TRAINER FUNCTIONS ** **/
-static void exit_with_help_message(char** argv) {
+static void exit_with_help_message(void) {
     puts("Primary syntax:");
     printf("  sct <hack> [enable|disable]\n");
     puts("Hacks available:");
@@ -82,13 +82,13 @@ static void parse_arguments(int argc, char** argv) {
             } else if (streq(argv[2], "disable")) {
                 g_mod_thermal_enable = false;
             } else {
-                exit_with_help_message(argv);
+                exit_with_help_message();
             }
         } else {
-            exit_with_help_message(argv);
+            exit_with_help_message();
         }
     } else {
-        exit_with_help_message(argv);
+        exit_with_help_message();
     }
 }
 
@@ -107,27 +107,75 @@ static size_t patch_level_flag(const char level_path[], long flag_offset, int fl
     return bytes_written;
 }
 
+static void patch_thermal(void) {
+    size_t bytes_written = 0;
+    if (g_mod_thermal_enable) {
+        bytes_written += patch_level_flag(level_path_training_2, NOTHERMAL_FLAG_OFFSET_TRAINING_2, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_patch_bytes);
+        bytes_written += patch_level_flag(level_path_training_3, NOTHERMAL_FLAG_OFFSET_TRAINING_3, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_patch_bytes);
+
+        bytes_written += patch_level_flag(level_path_tbilisi_0, NOTHERMAL_FLAG_OFFSET_TBILISI_0, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_patch_bytes);
+        bytes_written += patch_level_flag(level_path_tbilisi_1, NOTHERMAL_FLAG_OFFSET_TBILISI_1, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_patch_bytes);
+        bytes_written += patch_level_flag(level_path_tbilisi_2, NOTHERMAL_FLAG_OFFSET_TBILISI_2, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_patch_bytes);
+
+        bytes_written += patch_level_flag(level_path_defense_ministry_1, NOTHERMAL_FLAG_OFFSET_DEFENSE_MINISTRY_1,
+                                          NOTHERMAL_FLAG_SIZE, nothermal_flag_patch_bytes);
+        bytes_written += patch_level_flag(level_path_defense_ministry_2, NOTHERMAL_FLAG_OFFSET_DEFENSE_MINISTRY_2,
+                                          NOTHERMAL_FLAG_SIZE, nothermal_flag_patch_bytes);
+
+        bytes_written +=
+            patch_level_flag(level_path_caspian_oil_refinery_2, NOTHERMAL_FLAG_OFFSET_CASPIAN_OIL_REFINERY_2,
+                             NOTHERMAL_FLAG_SIZE, nothermal_flag_patch_bytes);
+        bytes_written +=
+            patch_level_flag(level_path_caspian_oil_refinery_3, NOTHERMAL_FLAG_OFFSET_CASPIAN_OIL_REFINERY_3,
+                             NOTHERMAL_FLAG_SIZE, nothermal_flag_patch_bytes);
+
+        if (bytes_written == NOTHERMAL_TOTAL_SIZE_TO_WRITE) {
+            puts("Enabled thermal hack");
+        }
+    } else {
+        bytes_written += patch_level_flag(level_path_training_2, NOTHERMAL_FLAG_OFFSET_TRAINING_2, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_original_bytes);
+        bytes_written += patch_level_flag(level_path_training_3, NOTHERMAL_FLAG_OFFSET_TRAINING_3, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_original_bytes);
+
+        bytes_written += patch_level_flag(level_path_tbilisi_0, NOTHERMAL_FLAG_OFFSET_TBILISI_0, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_original_bytes);
+        bytes_written += patch_level_flag(level_path_tbilisi_1, NOTHERMAL_FLAG_OFFSET_TBILISI_1, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_original_bytes);
+        bytes_written += patch_level_flag(level_path_tbilisi_2, NOTHERMAL_FLAG_OFFSET_TBILISI_2, NOTHERMAL_FLAG_SIZE,
+                                          nothermal_flag_original_bytes);
+
+        bytes_written += patch_level_flag(level_path_defense_ministry_1, NOTHERMAL_FLAG_OFFSET_DEFENSE_MINISTRY_1,
+                                          NOTHERMAL_FLAG_SIZE, nothermal_flag_original_bytes);
+        bytes_written += patch_level_flag(level_path_defense_ministry_2, NOTHERMAL_FLAG_OFFSET_DEFENSE_MINISTRY_2,
+                                          NOTHERMAL_FLAG_SIZE, nothermal_flag_original_bytes);
+
+        bytes_written +=
+            patch_level_flag(level_path_caspian_oil_refinery_2, NOTHERMAL_FLAG_OFFSET_CASPIAN_OIL_REFINERY_2,
+                             NOTHERMAL_FLAG_SIZE, nothermal_flag_original_bytes);
+        bytes_written +=
+            patch_level_flag(level_path_caspian_oil_refinery_3, NOTHERMAL_FLAG_OFFSET_CASPIAN_OIL_REFINERY_3,
+                             NOTHERMAL_FLAG_SIZE, nothermal_flag_original_bytes);
+
+        if (bytes_written == NOTHERMAL_TOTAL_SIZE_TO_WRITE) {
+            puts("Disabled thermal hack");
+        }
+    }
+    if (bytes_written != NOTHERMAL_FLAG_SIZE) {
+        puts("ERROR: Something went wrong during patch");
+    }
+}
+
 int main(int argc, char** argv) {
     puts("Splinter Cell Trainer");
     parse_arguments(argc, argv);
 
     if (g_mod_thermal) {
-        size_t bytes_written = {0};
-        if (g_mod_thermal_enable) {
-            bytes_written = patch_level_flag(level_path_training_2, NOTHERMAL_FLAG_OFFSET_TRAINING_2,
-                                             NOTHERMAL_FLAG_SIZE, nothermal_flag_patch_bytes);
-            if (bytes_written == NOTHERMAL_FLAG_SIZE) {
-                puts("Enabled thermal hack");
-            }
-        } else {
-            bytes_written = patch_level_flag(level_path_training_2, NOTHERMAL_FLAG_OFFSET_TRAINING_2,
-                                             NOTHERMAL_FLAG_SIZE, nothermal_flag_original_bytes);
-            if (bytes_written == NOTHERMAL_FLAG_SIZE) {
-                puts("Disabled thermal hack");
-            }
-        }
-        if (bytes_written != NOTHERMAL_FLAG_SIZE) {
-            puts("ERROR: Something went wrong during patch");
-        }
+        patch_thermal();
     }
 }
